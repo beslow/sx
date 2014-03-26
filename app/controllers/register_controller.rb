248fc24e627b6a7f3b29dbf20ba2d09f3484608a
beslow <549174542@qq.com>
2014-03-26 17:31:27 +0800

    3-26-17-31

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -7,8 +7,20 @@ class RegisterController < ApplicationController
     user.password = params[:register_form][:password]
     user.email = params[:register_form][:email]
     user.company = params[:register_form][:company]
+    begin
     user.save
+    rescue
+    end
     session[:user_id] = user.id
     redirect_to home_index_path
+  end
+  def validate_name_unique
+      project_templatemy =ProjectTemplate.find_by_id(params[:templatemy_id])
+      user = User.find_by(name: params[:name])
+      if user.blank?
+        render :inline => "<img src='/assert/true.png' />"
+      else
+        render :inline => "<img src='/assert/false.png' />"
+      end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  validates_uniqueness_of :name
   def self.login(name, password)
     hashed_password = hash_password(password || "")
     find(:first,:conditions => ["name = ? and hashed_password = ?",name, hashed_password])
